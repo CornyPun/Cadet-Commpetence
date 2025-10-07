30e9830c4a658357e8a860b56f9d607afa15fb22
CornyPun <nkl.courtney@gmail.com>
2025-10-06 21:55:22 -0600

    Weekly Watch Screen Prototype

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const mainContainer = document.querySelector(".main-container");
-const homeButtons = document.getElementsByClassName("home-button");
+const buttons = document.getElementsByClassName("button");
 
-function homeButtonClick(event) {
-    mainContainer.classList.add("main-container-2");
-
+function buttonMouseDown(event) {
     const button = event.currentTarget;
+    
+    if (button.classList.contains("home-button")) {
+        button.classList.add("home-button-pressed");
+        mainContainer.classList.add("main-container-2");
+    } else if (button.classList.contains("ww-button")) {
+        button.classList.add("ww-button-pressed");
+        mainContainer.classList.replace("main-container-2", "main-container-3")
+    }
 
     const circle = document.createElement("span");
     const diameter = Math.max(button.clientWidth, button.clientHeight);
     const radius = diameter / 2;
 
     circle.style.width = circle.style.height = `${diameter}px`;
+    
     circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
     circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
     circle.classList.add("ripple");
@@ -24,15 +31,6 @@ function homeButtonClick(event) {
     button.appendChild(circle);
 }
 
-function homeButtonMouseUp(event) {
-    const button = event.currentTarget;
-
-    button.classList.add("home-button-active-to-initial");
-
-    setTimeout(() => {button.classList.remove("home-button-active-to-initial")}, 600);
-}
-
-for (const homeButton of homeButtons) {
-    homeButton.addEventListener("click", homeButtonClick);
-    homeButton.addEventListener("mouseup", homeButtonMouseUp);
+for (const button of buttons) {
+    button.addEventListener("mousedown", buttonMouseDown);
 }
